ab0e847de20731deeba2debc924757243093c1f9
Addison Zheng <32391554+AddisonSubtract@users.noreply.github.com>
2022-10-17 16:38:13 -0500

    Mostly Finished

--- project2/MainPanel.java
+++ project2/MainPanel.java
@@ -33,23 +33,30 @@ public class MainPanel extends JPanel{
 	private JLabel lblNewLabel;
 	private Shows allData;
 	private final ButtonGroup buttonGroup = new ButtonGroup();
-	private JRadioButton rdbtnNewRadioButton;
 	private ImageIcon image, netflix;
 	private int x, y;
-	private String movieName = "Me Time";
-	private String showTitle = "";
-	
+	ShowWeek newShow = new ShowWeek();
+
+	private String week;
+	private String category;
+	private String rank;
+	private String showTitle;
+	private String seasonTitle;
+	private String hrsViewed;
+	private String weeksTop10;
+
 	public MainPanel() {
 		allData = new Shows("allData","./project1/netflixAllWeeksGlobalProcessed.txt");
 
 		netflix = new ImageIcon (this.getClass().getResource("/netflixIntro.gif"));
-		
+		//image = new ImageIcon (this.getClass().getResource("/MeTime.jpg"));
+
 		JPanel PowerSave = new JPanel();
 		PowerSave.setBackground(Color.BLACK);
 		PowerSave.setForeground(Color.BLACK);
-		PowerSave.setBounds(0, 33, 800, 375);
+		PowerSave.setBounds(0, 34, 800, 566);
 		add(PowerSave);
-		
+
 		JLabel moviePoster = new JLabel(" ");
 		moviePoster.setIcon(new ImageIcon("./MeTime.jpg"));
 		moviePoster.setBounds(518, 127, 215, 332);
@@ -61,12 +68,12 @@ public class MainPanel extends JPanel{
 		setPreferredSize(new Dimension(800, 600));
 
 		movieTitle = new JTextField("2022-09-04");
-		movieTitle.setBounds(68, 210, 149, 20);
+		movieTitle.setBounds(36, 183, 149, 20);
 		add(movieTitle);
 		movieTitle.setColumns(10);
 
 		lblNewLabel = new JLabel("Enter Week:");
-		lblNewLabel.setBounds(68, 198, 105, 14);
+		lblNewLabel.setBounds(36, 171, 105, 14);
 		add(lblNewLabel);
 
 		JScrollPane scrollPane = new JScrollPane();
@@ -88,11 +95,11 @@ public class MainPanel extends JPanel{
 		}
 
 		Movies.setModel(new DefaultComboBoxModel(data));
-		Movies.setBounds(68, 282, 149, 22);
+		Movies.setBounds(36, 255, 149, 22);
 		add(Movies);
 
 		JLabel lblNewLabel_1 = new JLabel("Shows:");
-		lblNewLabel_1.setBounds(68, 270, 46, 14);
+		lblNewLabel_1.setBounds(36, 241, 46, 14);
 		add(lblNewLabel_1);
 
 		JButton btnNewButton = new JButton("Get Shows");
@@ -111,42 +118,9 @@ public class MainPanel extends JPanel{
 			}
 		});
 
-		btnNewButton.setBounds(68, 230, 89, 23);
+		btnNewButton.setBounds(36, 203, 89, 23);
 		add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("Do Edit!");
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(rdbtnNewRadioButton.isSelected())
-				{
-//					try {
-//						String changeTitle = JOptionPane.showInputDialog("Enter a Title");
-//						movieTitle = Integer.parseInt(delayStr);
-//					} catch (Exception e) {
-//						delay = -1;
-//					}
-				}
-			}
-		});
-		btnNewButton_1.setBounds(128, 315, 89, 23);
-		add(btnNewButton_1);
-		
-		rdbtnNewRadioButton = new JRadioButton("Title");
-		rdbtnNewRadioButton.setSelected(true);
-		buttonGroup.add(rdbtnNewRadioButton);
-		rdbtnNewRadioButton.setBounds(13, 317, 109, 23);
-		add(rdbtnNewRadioButton);
-		
-		JRadioButton rdbtnWeek = new JRadioButton("Week");
-		buttonGroup.add(rdbtnWeek);
-		rdbtnWeek.setBounds(13, 392, 109, 23);
-		add(rdbtnWeek);
-		
-		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Purged");
-		buttonGroup.add(rdbtnNewRadioButton_1);
-		rdbtnNewRadioButton_1.setBounds(13, 355, 109, 23);
-		add(rdbtnNewRadioButton_1);
-		
+
 		JCheckBox chckbxNewCheckBox = new JCheckBox("Power Saving Mode");
 		chckbxNewCheckBox.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -160,112 +134,193 @@ public class MainPanel extends JPanel{
 			}
 		});
 		chckbxNewCheckBox.setSelected(true);
-		chckbxNewCheckBox.setBounds(13, 6, 120, 21);
+		chckbxNewCheckBox.setBounds(13, 6, 149, 21);
 		add(chckbxNewCheckBox);
-		
-		JButton btnNewButton_2 = new JButton("Poster 1");
-		btnNewButton_2.addActionListener(new ActionListener() {
+
+		JButton Poster1 = new JButton("Poster 1");
+		Poster1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				moviePoster.setIcon(new ImageIcon("./MeTime.jpg"));
 			}
 		});
-		btnNewButton_2.setBounds(495, 424, 85, 21);
-		add(btnNewButton_2);
-		
-		JButton btnNewButton_2_1 = new JButton("Poster 2");
-		btnNewButton_2.addActionListener(new ActionListener() {
+		Poster1.setBounds(212, 379, 85, 21);
+		add(Poster1);
+
+		JButton Poster2 = new JButton("Poster 2");
+		Poster2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				moviePoster.setIcon(new ImageIcon("./LoveInTheVilla.jpg"));
+			}
+		});
+
+		Poster2.setBounds(307, 379, 85, 21);
+		Poster2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				moviePoster.setIcon(new ImageIcon("./ICameBy.jpg"));
+			}
+		});
+
+		add(Poster2);
+
+		JButton Poster3 = new JButton("Poster 3");
+		Poster3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 
-		btnNewButton_2_1.setBounds(590, 424, 85, 21);
-		btnNewButton_2.addActionListener(new ActionListener() {
+		Poster3.setBounds(402, 379, 85, 21);
+		add(Poster3);
+
+		JButton Poster4 = new JButton("Poster 4");
+		Poster4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 
-		add(btnNewButton_2_1);
-		
-		JButton btnNewButton_2_2 = new JButton("Poster 3");
-		btnNewButton_2.addActionListener(new ActionListener() {
+		Poster4.setBounds(212, 410, 85, 21);
+		add(Poster4);
+
+		JButton Poster5 = new JButton("Poster 5");
+		Poster5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 
-		btnNewButton_2_2.setBounds(685, 424, 85, 21);
-		add(btnNewButton_2_2);
-		
-		JButton btnNewButton_2_3 = new JButton("Poster 4");
-		btnNewButton_2.addActionListener(new ActionListener() {
+		Poster5.setBounds(307, 410, 85, 21);
+		add(Poster5);
+
+		JButton Poster6 = new JButton("Poster 6");
+		Poster6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 
-		btnNewButton_2_3.setBounds(495, 455, 85, 21);
-		add(btnNewButton_2_3);
-		
-		JButton btnNewButton_2_4 = new JButton("Poster 5");
-		btnNewButton_2.addActionListener(new ActionListener() {
+		Poster6.setBounds(402, 410, 85, 21);
+		add(Poster6);
+
+		JButton Poster7 = new JButton("Poster 7");
+		Poster7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 
-		btnNewButton_2_4.setBounds(590, 455, 85, 21);
-		add(btnNewButton_2_4);
-		
-		JButton btnNewButton_2_5 = new JButton("Poster 6");
-		btnNewButton_2.addActionListener(new ActionListener() {
+		Poster7.setBounds(212, 445, 85, 21);
+		add(Poster7);
+
+		JButton Poster8 = new JButton("Poster 8");
+		Poster8.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 
-		btnNewButton_2_5.setBounds(685, 455, 85, 21);
-		add(btnNewButton_2_5);
-		
-		JButton btnNewButton_2_6 = new JButton("Poster 7");
-		btnNewButton_2.addActionListener(new ActionListener() {
+		Poster8.setBounds(307, 445, 85, 21);
+		add(Poster8);
+
+		JButton Poster9 = new JButton("Poster 9");
+		Poster9.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 
-		btnNewButton_2_6.setBounds(495, 490, 85, 21);
-		add(btnNewButton_2_6);
-		
-		JButton btnNewButton_2_7 = new JButton("Poster 8");
-		btnNewButton_2.addActionListener(new ActionListener() {
+		Poster9.setBounds(402, 445, 85, 21);
+		add(Poster9);
+
+		JButton Poster10 = new JButton("Poster 10");
+		Poster10.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 
-		btnNewButton_2_7.setBounds(590, 490, 85, 21);
-		add(btnNewButton_2_7);
-		
-		JButton btnNewButton_2_8 = new JButton("Poster 9");
-		btnNewButton_2.addActionListener(new ActionListener() {
+		Poster10.setBounds(307, 481, 85, 21);
+		add(Poster10);
+
+		JRadioButton rdbtnAdd = new JRadioButton("Add");
+		buttonGroup.add(rdbtnAdd);
+		rdbtnAdd.setBounds(13, 345, 109, 23);
+		add(rdbtnAdd);
+
+		JRadioButton rdbtnPurge = new JRadioButton("Remove");
+		buttonGroup.add(rdbtnPurge);
+		rdbtnPurge.setBounds(13, 384, 109, 23);
+		add(rdbtnPurge);
+
+		JRadioButton rdbtnEdit = new JRadioButton("Edit");
+		buttonGroup.add(rdbtnEdit);
+		rdbtnEdit.setBounds(13, 422, 109, 23);
+		add(rdbtnEdit);
+
+
+
+
+		JButton editButton = new JButton("Do Edit!");
+		editButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				if(rdbtnAdd.isSelected())
+				{
+					week = JOptionPane.showInputDialog("Enter a Week");
+					category = JOptionPane.showInputDialog("Enter a Category");
+					rank = JOptionPane.showInputDialog("Enter a Rank");
+					showTitle = JOptionPane.showInputDialog("Enter a Title");
+					seasonTitle = JOptionPane.showInputDialog("Enter a Season Title");
+					hrsViewed = JOptionPane.showInputDialog("Enter Hours");
+					weeksTop10 = JOptionPane.showInputDialog("Enter Weeks in Top ");
+
+					newShow.setWeek(week);
+					newShow.setCategory(category);
+					newShow.setRank(rank);
+					newShow.setShowTitle(showTitle);
+					newShow.setSeasonTitle(seasonTitle);
+					newShow.setHrsViewed(hrsViewed);
+					newShow.setWeeksTop10(weeksTop10);
+
+					allData.addShowWeek(newShow);
+				}
+				else if(rdbtnPurge.isSelected())
+				{
+					showTitle = JOptionPane.showInputDialog("Enter a Title");
+					allData.purgeShow(showTitle);
+					
+				}else if(rdbtnEdit.isSelected())
+				{
+					showTitle = JOptionPane.showInputDialog("Enter a Title");
+					week = JOptionPane.showInputDialog("Enter a Week");
+
+					ShowWeek editShowWeek = allData.find(showTitle, week);
+
+					week = JOptionPane.showInputDialog("Enter a Week");
+					category = JOptionPane.showInputDialog("Enter a Category");
+					rank = JOptionPane.showInputDialog("Enter a Rank");
+					showTitle = JOptionPane.showInputDialog("Enter a Title");
+					seasonTitle = JOptionPane.showInputDialog("Enter a Season Title");
+					hrsViewed = JOptionPane.showInputDialog("Enter Hours");
+					weeksTop10 = JOptionPane.showInputDialog("Enter Weeks in Top ");
+
+					editShowWeek.setWeek(week);
+					editShowWeek.setCategory(category);
+					editShowWeek.setRank(rank);
+					editShowWeek.setShowTitle(showTitle);
+					editShowWeek.setSeasonTitle(seasonTitle);
+					editShowWeek.setHrsViewed(hrsViewed);
+					editShowWeek.setWeeksTop10(weeksTop10);
+
+					allData.addShowWeek(editShowWeek);
+				}
 			}
 		});
+		editButton.setBounds(128, 345, 89, 23);
+		add(editButton);
 
-		btnNewButton_2_8.setBounds(685, 490, 85, 21);
-		add(btnNewButton_2_8);
-		
-		JButton btnNewButton_2_9 = new JButton("Poster 10");
-		btnNewButton_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
 
-		btnNewButton_2_9.setBounds(590, 526, 85, 21);
-		add(btnNewButton_2_9);
 	}
 
 	public void doClose() {
-		allData.doWrite("./textwrite.csv");
+		allData.doWrite("./textwrite.txt");
 	}
 
 	public void paintComponent (Graphics page)
 	{
 		super.paintComponent (page);
-		//image.paintIcon (this, page, x, y);
+		//image.paintIcon (this, page, 500, 300);
 		netflix.paintIcon (this, page, 365, -150);
 	}
 }
